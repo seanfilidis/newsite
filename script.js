441ca702e093697f414ce4aee22bf340c0a97894
seanfilidis <seanfilidis@gmail.com>
2019-11-10 12:48:58 +0100

    add color change script

---
+++ script.js
@@ -0,0 +1,8 @@
+function toRed() {
+    var title = document.getElementById('title');
+    if (title.style.color == 'red') {
+        title.style.color = 'black';
+    } else {
+        title.style.color = 'red'
+    }
+}
