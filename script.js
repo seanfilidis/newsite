afa428fb9392df3f52946bf54cd426223c001c7b
seanfilidis <seanfilidis@gmail.com>
2019-11-10 12:59:36 +0100

    added blue toggle

--- script.js
+++ script.js
@@ -6,3 +6,12 @@ function toRed() {
         title.style.color = 'red'
     }
 }
+
+function toBlue() {
+    var title = document.getElementById('title');
+    if (title.style.color == 'blue') {
+        title.style.color = 'black';
+    } else {
+        title.style.color = 'blue'
+    }
+}
