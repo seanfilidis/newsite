2901ca70982b78c761d10f960f29b6baf2b2f727
seanfilidis <seanfilidis@gmail.com>
2019-11-13 22:44:22 +0100

    Add sticky sidebar, fixed color switcher

--- script.js
+++ script.js
@@ -1,19 +1,14 @@
 function changeColor(color) {
-    var title = document.getElementsByTagName('h2');
-    function changeThem() {
-        if (title.style.color == color) {
-            title.style.color = 'black';
-        } else if (color == 'red') {
-            title.style.color = 'red';
-        } else if (color == 'blue') {
-            title.style.color = 'blue';
-        } else if (color == 'green') {
-            title.style.color = 'green';
-        }
+    var title = document.getElementById('title');
+    if (title.style.color == color) {
+        title.style.color = 'black';
+    } else if (color == 'red') {
+        title.style.color = 'red';
+    } else if (color == 'blue') {
+        title.style.color = 'blue';
+    } else if (color == 'green') {
+        title.style.color = 'green';
     }
-    title.forEach(changeThem);
-        
-    });
 }
 
 function scrolllTo(number) {
