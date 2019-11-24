b4d2a60e5be07c44491cdf098916193fe36afaff
seanfilidis <seanfilidis@gmail.com>
2019-11-24 11:28:27 +0100

    Add theme toggle

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 function changeColor(color) {
+    /* this function changes just the title, could be made simpler
     var title = document.getElementById('title');
-    if (title.style.color == color) {
+    if (color == 'black') {
         title.style.color = 'black';
     } else if (color == 'red') {
         title.style.color = 'red';
@@ -8,6 +9,12 @@ function changeColor(color) {
         title.style.color = 'blue';
     } else if (color == 'green') {
         title.style.color = 'green';
+    }*/
+    var myBody = document.getElementsByTagName("body");
+    if (color == 'black') {
+        myBody[0].className = '';
+    } else {
+        myBody[0].className = color;
     }
 }
 
