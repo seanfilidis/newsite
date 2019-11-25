5efc7fbfe5544022ad97830d686af65aca28cb5f
seanfilidis <seanfilidis@gmail.com>
2019-11-25 16:36:36 +0100

    Add back to top link. Update text.

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ function changeColor(color) {
     }
 }
 
-function scrolllTo(number) {
+function scrolllTo(number) { // Scrolls to a location taking into account the sticky header
     var headerOffset = 55;
     var target = document.getElementById(number);
     var bodyRect = document.body.getBoundingClientRect().top;
@@ -22,11 +22,12 @@ function scrolllTo(number) {
 
 document.addEventListener("scroll", navBg);
 
-function navBg() {
+function navBg() { // Does stuff depending how far down the page you scroll
     var depth = window.pageYOffset;
     var navcolor = document.getElementById("topnav");
     var trans = document.getElementById('transparent');
     var scrollIndicator = document.getElementById('scroll-indicator');
+    var backToTop = document.getElementById('back-to-top');
     if (depth < 2) {
         navcolor.className = 'nav-transparent';
         trans.innerHTML = 'Transparent';
@@ -35,5 +36,10 @@ function navBg() {
         navcolor.className = 'nav-colored';
         trans.innerHTML = 'Opaque';
         scrollIndicator.style.opacity = 0;
+    }
+    if (depth < 650) {
+        backToTop.style.display = 'none';
+    } else {
+        backToTop.style.display = 'inline';
     }
 }
